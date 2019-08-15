ba56983a32d210244d02cbe556acd1b0c8ab216b
IT302 <mypenguinkung@gmail.com>
2019-08-15 14:29:28 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Io Application'),
+      home: MyHomePage(title: 'IoT Application'),
     );
   }
 }
